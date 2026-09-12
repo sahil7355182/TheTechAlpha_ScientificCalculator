53e420ef1a95ed8ca5fad907cc160fe539c18855
Sahil <sahilsingh735574@gmail.com>
2026-09-12 13:07:12 +0530

    Add files via upload

---
+++ scientific_calculator.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+{
+    float N1, N2, result;
+    char operater;
+
+    printf("Enter first number: ");
+    scanf("%f", &N1);
+
+    printf("Enter operator: ");
+    scanf(" %c", &operater);
+
+    printf("Enter second number: ");
+    scanf("%f", &N2);
+
+    if (operator== '+')
+    {
+        result = N1+N2;
+        printf("Result = %.2f", result);
+    }
+    else if (operator== '-')
+    {
+        result = N1-N2;
+        printf("Result = %.2f", result);
+    }
+    else if (operator== '*')
+    {
+        result = N1 * N2;
+        printf("Result = %.2f", result);
+    }
+    else if (operator== '/')
+    {
+        if (N2 == 0)
+        {
+            printf("Error: Cannot divide by zero.");
+        }
+        else
+        {
+            result = N1 /N2;
+            printf("Result = %.2f", result);
+        }
+    }
+    else
+    {
+        printf("Invalid operator.");
+    }
+    return 0;
+}
